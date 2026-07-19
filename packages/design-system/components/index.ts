9267ea570b326f9ba4782dd52cff7d3dd757ee15
dd-founer-office <devotionmanagers@gmail.com>
2026-07-19 11:22:30 +0400

    Add ProgressIndicator and PhotoGallery exports

--- packages/design-system/components/index.ts
+++ packages/design-system/components/index.ts
@@ -2,3 +2,5 @@ export * from "./Button/Button";
 export * from "./Card/Card";
 export * from "./Badge/Badge";
 export * from "./Chip/Chip";
+export * from "./ProgressIndicator/ProgressIndicator";
+export * from "./PhotoGallery/PhotoGallery";
